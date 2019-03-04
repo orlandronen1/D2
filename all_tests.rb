85eb994827cec10bc8f2ae17ebe6c825bd62a87d
Unknown <roo18@pitt.edu>
2019-03-04 16:59:44 -0500

    Made Town class
    Created Town class and its methods

--- all_tests.rb
+++ all_tests.rb
@@ -2,3 +2,4 @@ require 'simplecov'
 SimpleCov.start
 
 # require_relative 'test_file_name'
+require_relative 'town_test'
---
+++ town.rb
@@ -0,0 +1,45 @@
+class Town
+
+	attr_reader :neighbors
+
+	# Initializes the town with name, max ruby, max fake ruby, seed, and neighbors
+	def initialize n, max_r, max_faker, seed
+		@name = n
+		@max_rubies = max_r
+		@max_fake_rubies = max_faker
+		@random = Random.new(seed)
+		# Initialize neighbors to be empty list
+		@neighbors = []
+	end
+
+	# Returns the name of the town
+	def name
+		@name
+	end
+
+	# Returns the number of neighboring Towns
+	def num_neighbors
+		@neighbors.count
+	end
+
+	# Adds a neighboring Town (not needed if initializing w/ neighbors)
+	def add_neighbor n
+		@neighbors << n
+	end
+
+	# Returns a psuedorandomly determined neighbor
+	def next
+		@neighbors[@random.rand(neighbors.count)]
+	end
+
+	# Returns a psuedorandomly determined amount of rubies found
+	def find_rubies
+		# Add 1 to max_rubies to make maximum inclusive
+		@random.rand(@max_rubies + 1)
+	end
+
+	# Returns a psuedorandomly determined amount of fake rubies found
+	def find_fake_rubies
+		# Add 1 to max_fake_rubies to make maximum inclusive
+		@random.rand(@max_fake_rubies + 1)
+	end
